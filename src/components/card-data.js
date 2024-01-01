c655d1717575a3d53cb96f3af2501ed7fcfdb76e
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-01 08:53:20 -0800

    Add fetchCardData with Unsplash API call

--- src/components/card-data.js
+++ src/components/card-data.js
@@ -1,8 +1,22 @@
 let cardDataAr = [];
-const apiKey = 'DuRXDS10VCH_SxhpAsE3VUNPhkk57SvrOAahsmsv2ds';
+const apiKey = 'client_id=DuRXDS10VCH_SxhpAsE3VUNPhkk57SvrOAahsmsv2ds';
+const apiLink = 'https://api.unsplash.com/photos/';
 
 const getCardDataAr = () => {
     return cardDataAr;
 };
 
-export { getCardDataAr };
+const fetchCardData = (theme = 'random') => {
+    let photoUrl = '';
+
+    fetch(`${apiLink}${theme}/?${apiKey}`, { mode: 'cors' })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            photoUrl = data.urls.small;
+        })
+        .catch(error => console.log(error));
+};
+
+export { getCardDataAr, fetchCardData };
