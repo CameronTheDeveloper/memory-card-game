8e2d73797d6f22afd2175f43c5f5fc4c2913e794
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-04 06:54:11 -0800

    Fetch 12 photos and store in cardDataAr

--- src/components/card-data.js
+++ src/components/card-data.js
@@ -1,20 +1,18 @@
 let cardDataAr = [];
 const apiKey = 'client_id=DuRXDS10VCH_SxhpAsE3VUNPhkk57SvrOAahsmsv2ds';
-const apiLink = 'https://api.unsplash.com/photos/';
+const apiLink = 'https://api.unsplash.com/search/photos';
 
 const getCardDataAr = () => {
     return cardDataAr;
 };
 
-const fetchCardData = (theme = 'random') => {
-    let photoUrl = '';
-
-    fetch(`${apiLink}${theme}/?${apiKey}`, { mode: 'cors' })
+const fetchCardData = (theme = 'tiger') => {
+    fetch(`${apiLink}?query=${theme}&per_page=12&${apiKey}`, { mode: 'cors' })
         .then(response => {
             return response.json();
         })
         .then(data => {
-            photoUrl = data.urls.small;
+            cardDataAr.push(data.results);
         })
         .catch(error => console.log(error));
 };
