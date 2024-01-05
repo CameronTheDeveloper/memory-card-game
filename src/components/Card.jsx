dfca836ddd5c88068f2b10be3364ef7ebcc4be7a
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-05 10:03:19 -0800

    Refactor Card component to return button element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ children }) {
     return (
-        <div className="card">
+        <button className="card">
             {children}
-        </div>
+        </button>
     );
 }
