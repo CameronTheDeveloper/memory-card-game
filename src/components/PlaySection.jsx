8c87d2e5f18f946309a389be819db38fb92cb8a8
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-05 06:02:09 -0800

    Fetch & set cardData with an effect

--- src/components/PlaySection.jsx
+++ src/components/PlaySection.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CardContainer from "./CardContainer";
+import { fetchCardData } from "./card-data";
 
 export default function PlaySection() {
     const [cardData, setCardData] = useState([]);
+
+    useEffect(() => {
+        let ignore = false;
+        ignore == false && setCardData(fetchCardData);
+        return () => {
+            ignore = true;
+        };
+    }, [cardData]);
+
     return (
         <div id='play-section'>
             <CardContainer cardDataAr={cardData}></CardContainer>
