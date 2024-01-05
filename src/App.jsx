bd8f5a69adffa61a3295137973d4fc9898d3cb11
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-05 05:32:41 -0800

    Remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import PlaySection from './components/PlaySection';
-import { fetchCardData } from './components/card-data';
 import './App.css';
 
 function App() {
-  //Need to call fetchCardData once outside CardContainer
   return (
     <>
       <PlaySection></PlaySection>
