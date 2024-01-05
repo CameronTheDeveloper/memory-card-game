1850d1f24d3451041be91704511a199d7255d2f9
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-05 13:32:42 -0800

    Keep track of selected cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export default function Card({ addScore, children }) {
+    const [selected, setSelected] = useState(false);
+
     function handleClick() {
-        addScore();
+        if (!selected) {
+            addScore();
+            setSelected(true);
+        } else {
+            console.log('Lose');
+        }
     }
+
     return (
         <button onClick={handleClick} className="card">
             {children}
