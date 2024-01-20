862cb5fbff7a9d3984b80dc7e94ee0052ab96fd6
Cameron Posey <cameronposey@outlook.com>
2024-01-19 16:30:50 -0800

    Check for status errors in card data

--- src/components/card-data.js
+++ src/components/card-data.js
@@ -7,6 +7,11 @@ const fetchCardData = async (theme = "tiger", setCardData, setLoading, setError)
       `${apiLink}?query=${theme}&per_page=12&${apiKey}`,
       { mode: "cors" },
     );
+    if (!response.ok){
+      throw new Error(
+        `An HTTP error occured: The status is ${response.status}`
+      );
+    }
     const data = await response.json();
     setCardData(data.results);
   } catch (error) {
